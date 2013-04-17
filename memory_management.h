b01bd5bf0a971c7e23f00e9128aab7a97ca0e590
Nathanael Schilling <teline.natschil@spamgourmet.com>
2013-04-17 19:54:40 +0200

    Some more documentation

--- memory_management.h
+++ memory_management.h
@@ -3,7 +3,7 @@
 // This file contains functions for (somewhat) efficiently allocating large amounts of memory
 // but without the ability to free small amounts of memory.
 
-//The global constants below should be modified before actually running.
+//The global constant MEMORY_PAGE_SIZE and NUM_PAGES below should be modified before actually running.
 //The N-gram counter uses buffers to store word-ngrams. However, because the stl map containers used
 //to keep track n-grams do not use the buffers for memory, actual memory usage is higher.
 
@@ -19,10 +19,12 @@
 //Set this to half of the actual number of "pages" used. 
 //4M per memory "page" size.
 #define MEMORY_PAGE_SIZE (4*1024*1024)
+//Set this to the number of "pages used"
 #define NUM_PAGES 100
 //Because we use two buffers that take up MEMORY_PAGE_SIZE*NUM_PAGES memory
 //Therefore
 #define TOTAL_MEM_USED MEMORY_PAGE_SIZE * 2 * NUM_PAGES
+//Note that the variable above is used only for documentation and should not be changed.
 
 void init_permanent_malloc(void(*)(void));
 extern volatile size_t current_page_group;
