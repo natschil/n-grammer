412b7710dc0e55da2a50b87bf3e89c6a8b9ea4ee
Nathanael Schilling <teline.natschil@spamgourmet.com>
2013-04-23 16:10:31 +0200

    Changed tester utility to match directory structure

--- run_largescale_tests.sh
+++ run_largescale_tests.sh
@@ -3,8 +3,13 @@ mkdir -p ./tests_output
 
 if [[ ! -a $1 ]]; then echo "Usage:" $0 "corpus_to_analyze";exit;fi;
 for i in `seq 1 6`; do
-	./ngram.counting $i $1 > ./tests_output/`basename $1`.${i}.output 2>/dev/null
-	if ! diff ./tests_output/`basename $1`.${i}.output ./reference_data/`basename $1`.${i}.output;
+	./ngram.counting $i $1 ./tests_output/`basename $1`.${i}.d 2>/dev/null
+
+	printf "" > ./tests_output/`basename $1`.${i}.output
+	firststring=""
+	for k in `seq 0 $((i-1))`; do firststring=${firststring}_${k};done;
+	for j in `seq 0 255`; do cat ./tests_output/`basename $1`.${i}.d/by${firststring}/${j}.out >> ./tests_output/`basename $1`.${i}.output;done;
+	if ! cmp ./tests_output/`basename $1`.${i}.output ./reference_data/`basename $1`.${i}.output;
 	then echo "Failed on try" $i;
 	else echo "Succeeded on try" $i;
 	fi;
