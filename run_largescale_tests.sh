8564ecc13b73f96a508291c13d1e246c01339c5a
Nathanael Schilling <teline.natschil@spamgourmet.com>
2013-05-02 23:35:40 +0200

    Better tester

--- run_largescale_tests.sh
+++ run_largescale_tests.sh
@@ -5,21 +5,31 @@ if [[ ! -a $1 ]]; then echo "Usage:" $0 "corpus_to_analyze";exit;fi;
 for i in `seq 1 6`; do
 	rm -rf ./tests_output/`basename $1`.${i}.d
 	rm -rf ./tests_output/`basename $1`.${i}.d
-	./ngram.counting $i $1 ./tests_output/`basename $1`.${i}.d >/dev/null
-
-	printf "" > ./tests_output/`basename $1`.${i}.output
-	firststring=""
-	for k in `seq 0 $((i-1))`; do firststring=${firststring}_${k};done;
-	for j in `seq 0 255`;
-	do 
-			if [ -f ./tests_output/`basename $1`.${i}.d/by${firststring}/${j}.out ];
-       			then 
-				cat   ./tests_output/`basename $1`.${i}.d/by${firststring}/${j}.out >> ./tests_output/`basename $1`.${i}.output;
-			fi;
-	done;
-
-	if ! cmp ./tests_output/`basename $1`.${i}.output ./reference_data/`basename $1`.${i}.output;
-	then echo "Failed on try" $i;
-	else echo "Succeeded on try" $i;
+	retval=$(./ngram.counting $i $1 ./tests_output/`basename $1`.${i}.d --wordsearch-index-depth=${i} >/dev/null)
+	if [ $retval ];
+	then
+		echo "Failed to run ngram-counter data";
+		exit 1;
 	fi;
+
+	for current_directory in $(ls -d ./tests_output/`basename $1`.${i}.d/*/);
+	do
+		filename=./tests_output/`basename $current_directory`.out;
+		printf "" > ${filename}
+		firststring=""
+		for j in `seq 0 255`;
+		do 
+				if [ -f ${current_directory}/${j}.out ];
+       				then 
+					cat   ${current_directory}/${j}.out >> $filename;
+				fi;
+		done;
+		if cmp ./tests_output/`basename $current_directory`.out ./reference_data/`basename $current_directory`.out;
+		then
+			echo "Succeeded on try $filename";
+		else
+			echo "Failed on try $filename";
+			exit -1;
+		fi;
+	done;
 done;
