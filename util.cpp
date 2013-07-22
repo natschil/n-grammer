289df081e5b763c12b633f22a7f6f7c5ac671fa5
Nathanael Schilling <teline.natschil@spamgourmet.com>
2013-07-22 17:30:51 +0200

    Various utility functions (such as split, join, functional_map)

---
+++ util.cpp
@@ -0,0 +1,47 @@
+#include "util.h"
+
+
+vector<string> split_ignoring_empty(const string& input_string,const char delim)
+{
+	istringstream ss(input_string);
+	string cur;
+	vector<string> result;
+	while(getline(ss,cur,delim))
+	{
+		if(!cur.empty())
+			result.push_back(cur);
+	}
+	if(!cur.empty())
+		result.push_back(cur);
+	return result;
+}
+
+vector<string> split(const string &input_string, const char delim)
+{
+	istringstream ss(input_string);
+	string cur;
+	vector<string> result;
+	while(getline(ss,cur,delim))
+	{
+		result.push_back(cur);
+	}
+	if(!cur.empty())
+		result.push_back(cur);
+	return result;
+}
+
+string join(const vector<string> &to_join,const string &delim)
+{
+	ostringstream result{""};
+	for(size_t i = 0; i < to_join.size(); i++)
+	{
+		result<<to_join[i];
+		if((i+1) != to_join.size())
+		{
+			result<<delim;
+		}
+	}
+	return result.str();
+}
+
+
---
+++ util.h
@@ -0,0 +1,45 @@
+//(c) 2013 Nathanael Schilling
+//This file contains various utility functions
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <functional>
+
+#ifndef NGRAM_ANALYSIS_UTILS
+#define NGRAM_ANALYSIS_UTILS
+using namespace std;
+vector<string> split_ignoring_empty(const string& inputstring,const char delim);
+vector<string> split(const string& inputstring);
+string join(const vector<string> &to_join,const string &delim);
+
+//Equivalent to a function that inductively defined, is so that:
+//		find_nth(a,end, value,(n+1)) = find(find_nth(a,end,value,n),end,value);
+//     and
+//     		find_nth(begin,end,value,1) = find(begin,end,value)
+
+template<class InputIt,class T> 
+InputIt find_nth(InputIt begin, InputIt end, const T& value,size_t n)
+{
+	InputIt result = begin;
+	while(n-- != 0)
+	{
+		find(result,end,value);
+	}
+	return result;
+}
+
+//Takes as parameters a vector<T> and mapfunction where mapfunction[T] gives U.
+//Returns a vector<U> so that for each element i output[i] =  mapfunction(input[i])
+template<class T,class U>
+vector<U> functional_map(vector<T> input, function<U(T)> mapper)
+{
+	vector<U> result;
+	for(auto &i : input)
+	{
+		result.push_back(mapper(i));
+	}
+	return result;
+}
+
+#endif
